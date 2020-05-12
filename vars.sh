06654296addcfe45e4f7cb5b5eb583a8ecad7531
Hariharan Parthiban <59941004+StealthAdder@users.noreply.github.com>
2020-05-13 00:07:50 +0530

    Add files via upload
    Variables

---
+++ vars.sh
@@ -0,0 +1,14 @@
+#
+#!/bin/bash
+
+pen_cost=10
+book_cost=30
+
+echo "cost of book is "$book_cost
+echo "cost of pen is "$pen_cost
+
+#encapsulating with "" will preserve white spaces values
+
+printme="HA	HA HA"
+echo $printme "other one $printme"
+#output: HA HA HA other one HA	HA HA
