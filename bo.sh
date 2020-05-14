18b4d0c5c518a4894709e359980c290d720d5f89
Hariharan Parthiban <59941004+StealthAdder@users.noreply.github.com>
2020-05-14 18:58:49 +0530

    Basic operations
    Basic operations in shell.

---
+++ bo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#basic operations.
+
+a=3
+b=3
+echo "a=3 and b=3"
+add=$(($a + $b))
+sub=$(($a - $b))
+mul=$(($a * $b))
+div=$(($a / $b))
+
+echo "add= "$add
+echo "sub= "$sub
+echo "mul= "$mul
+echo "div= "$div
